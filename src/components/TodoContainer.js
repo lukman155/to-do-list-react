d4d0360a4051fe6ede471b44a168f41bf40ca0b1
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2022-12-05 14:51:19 +0100

    Finish: Set up

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+class TodoContainer extends React.Component {
+  state = {
+      todos: [
+        {
+          id: 1,
+          title: "Setup development environment",
+          completed: true
+        },
+        {
+          id: 2,
+          title: "Develop website and add content",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "Deploy to live server",
+          completed: false
+        }
+      ]
+      };
+      
+  render() {
+      return (
+          <React.Fragment>
+              <h1>Hello from Create React App</h1>
+              <p>I am a React Component</p>
+          </React.Fragment>
+      )
+  }
+}
+
+export default TodoContainer;
